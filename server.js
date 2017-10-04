32ea43308b4eb48a7df9f3f32a661e731055e9d2
bluearcontech <blueskymobile@hotmail.com>
2017-10-05 01:47:22 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.send('<p>Hello</p>')
+});
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
