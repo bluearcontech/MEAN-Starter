8d9832872a1e538190f2ccf6c005c246de4a2825
bluearcontech <blueskymobile@hotmail.com>
2017-10-05 04:30:07 +0800

    implement log in functionlaity

--- app/app.js
+++ app/app.js
@@ -1,38 +1,43 @@
-angular
-  .module('app', ['ui-router'])
-  .config(config)
-  .run(run);
+(function () {
+  'use strict';
+
+  angular
+    .module('app', ['ui.router'])
+    .config(config)
+    .run(run);
 
   function config($stateProvider, $urlRouterProvider) {
+    // default route
     $urlRouterProvider.otherwise("/");
 
     $stateProvider
       .state('home', {
-        url:'/',
-        template: '<h1>Home</h1>'
+        url: '/',
+        templateUrl: 'home/index.html',
+        controller: 'Home.IndexController',
+        controllerAs: 'vm',
+        data: { activeTab: 'home' }
       })
-      .state('account', {
-        url: '/account',
-        template: '<h1>Account</h1>'
-      });
 
-    function run($http, $rootScope, $window) {
-        // add JWT token as default auth header
-        $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+  }
 
-        // update active tab on state change
-        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-            $rootScope.activeTab = toState.data.activeTab;
-        });
-    }
+  function run($http, $rootScope, $window) {
+    // add JWT token as default auth header
+    $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
 
-    // manually bootstrap angular after the JWT token is retrieved from the server
-    $(function () {
-        // get JWT token from server
-        $.get('/app/token', function (token) {
-            window.jwtToken = token;
-
-            angular.bootstrap(document, ['app']);
-        });
+    // update active tab on state change
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+      $rootScope.activeTab = toState.data.activeTab;
     });
   }
+
+  // manually bootstrap angular after the JWT token is retrieved from the server
+  $(function () {
+    // get JWT token from server
+    $.get('/app/token', function (token) {
+      window.jwtToken = token;
+
+      angular.bootstrap(document, ['app']);
+    });
+  });
+})();
--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,9 +5,7 @@ var config = require('config.json');
 
 router.get('/', function (req, res) {
     // log user out
-    if(req.session.token) {
-      delete req.session.token;
-    }
+    delete req.session.token;
 
     // move success message into local variable so it only appears once (single read)
     var viewData = { success: req.session.success };
@@ -16,4 +14,28 @@ router.get('/', function (req, res) {
     res.render('login', viewData);
 });
 
-module.exports = router
+router.post('/', function (req, res) {
+    // authenticate using api to maintain clean separation between layers
+    request.post({
+        url: config.apiUrl + '/users/authenticate',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.render('login', { error: 'An error occurred' });
+        }
+
+        if (!body.token) {
+            return res.render('login', { error: body, username: req.body.username });
+        }
+
+        // save JWT token in the session to make it available to the angular app
+        req.session.token = body.token;
+
+        // redirect to returnUrl
+        var returnUrl = req.query.returnUrl && decodeURIComponent(req.query.returnUrl) || '/';
+        res.redirect(returnUrl);
+    });
+});
+
+module.exports = router;
